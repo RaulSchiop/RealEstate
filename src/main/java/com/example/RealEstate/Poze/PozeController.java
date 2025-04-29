30c3c2a3d3cd5f8cb76fbcc17f6a0937b115b23f
RaulSchiop <raul.schiop@gmail.com>
2025-04-29 16:35:41 +0300

    photo controller

--- src/main/java/com/example/RealEstate/Poze/PozeController.java
+++ src/main/java/com/example/RealEstate/Poze/PozeController.java
@@ -1,4 +1,16 @@
 package com.example.RealEstate.Poze;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PozeController {
+    private final PozeServices pozeServices;
+
+    @Autowired
+    public PozeController(PozeServices pozeServices) {
+        this.pozeServices = pozeServices;
+    }
 }
+
