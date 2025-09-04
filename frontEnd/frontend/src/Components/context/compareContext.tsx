59b5635d7a5154a5413fde9f0c8690962f06b07a
RaulSchiop <raul.schiop@gmail.com>
2025-09-04 18:47:24 +0300

    type changes

---
+++ frontEnd/frontend/src/Components/context/compareContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext, ReactNode } from "react";
+
+type CompareContextType = {
+   id: number[];
+   addCompare: (id: number) => void;
+   deleteCompare: (id: number) => void;
+};
+
+export const compareContext = createContext<CompareContextType>({
+   id: [],
+   addCompare: () => {},
+   deleteCompare: () => {},
+});
+export function useCompareContxt() {
+   return useContext(compareContext);
+}
+
+type CompareProviderProps = {
+   children: ReactNode;
+};
+
+export function CompareContextProvider({ children }: CompareProviderProps) {
+   const [id, setId] = useState<number[]>([]);
+   function addCompare(id: number) {
+      setId((prev) => [...prev, id]);
+   }
+
+   function deleteCompare(id: number) {
+      setId((prev) => prev.filter((item) => item !== id));
+   }
+
+   return (
+      <compareContext.Provider value={{ id, addCompare, deleteCompare }}>
+         {children}
+      </compareContext.Provider>
+   );
+}
